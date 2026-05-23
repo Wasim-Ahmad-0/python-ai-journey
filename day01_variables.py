4877ec51eae934ba85057dcd119cd95c824ecfec
Wasim-Ahmad-0 <113457325+Wasim-Ahmad-0@users.noreply.github.com>
2026-05-23 10:47:13 +0530

    Add Day 1 - variables and strings exercise

---
+++ day01_variables.py
@@ -0,0 +1,7 @@
+# writing the code to get the name and of the user and print the name and age + 10 as a result
+
+user_name = input("What is your name? ")
+user_age = int(input("What is your age? "))
+
+print(f"My name is {user_name} and I will be {user_age + 10} in ten years")
+# this will print the name and the age after ten years
