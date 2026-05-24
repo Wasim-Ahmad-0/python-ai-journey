638c639afeb4d121ec36644fd39fa2b64d65261d
Wasim-Ahmad-0 <113457325+Wasim-Ahmad-0@users.noreply.github.com>
2026-05-24 15:07:18 +0530

    Trim whitespace and format user name input

--- day01_variables.py
+++ day01_variables.py
@@ -1,6 +1,6 @@
 # writing the code to get the name and of the user and print the name and age + 10 as a result
 
-user_name = input("What is your name? ")
+user_name = input("What is your name? ").strip().title()
 user_age = int(input("What is your age? "))
 
 print(f"My name is {user_name} and I will be {user_age + 10} in ten years")
