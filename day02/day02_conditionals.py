b2cd112c388e1528ed8efd4711eafded9c75dadb
Wasim-Ahmad-0 <113457325+Wasim-Ahmad-0@users.noreply.github.com>
2026-06-21 11:03:45 +0530

    Add input validation and error handling for grades
    Earlier it had flaws if our input was not between 1-100. It is fixed now.

--- day02/day02_conditionals.py
+++ day02/day02_conditionals.py
@@ -1,14 +1,21 @@
 # Determine student grades based on their test marks
 
-marks = int(input("What marks did you get? "))
+try:
+    marks = int(input("What marks did you get? "))
+    
+    if marks < 0 or marks > 100:
+        print("Please enter a valid mark between 0 and 100.")
+    else:
+        if marks >= 80:
+            grade, message = "A", "You have passed the test"
+        elif marks >= 60:
+            grade, message = "B", "You can do better"
+        elif marks >= 40:
+            grade, message = "C", "You need to improve"
+        else:
+            grade, message = "D", "You have failed"
 
-if marks >= 80:
-    grade, message = "A", "You have passed the test"
-elif marks >= 60:
-    grade, message = "B", "You can do better"
-elif marks >= 40:
-    grade, message = "C", "You need to improve"
-else:
-    grade, message = "D", "You have failed"
+        print(f"{message}, you got {grade}.")
 
-print(f"{message}, you got {grade}.")
+except ValueError:
+    print("Invalid input! Please enter a whole number.")
