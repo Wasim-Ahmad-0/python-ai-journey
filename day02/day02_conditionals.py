a9973a7916dec13374d344afa5a15db9d75bfd8d
Wasim-Ahmad-0 <113457325+Wasim-Ahmad-0@users.noreply.github.com>
2026-06-21 11:14:44 +0530

    Refactor grade calculation logic in conditionals.py

--- day02/day02_conditionals.py
+++ day02/day02_conditionals.py
@@ -22,24 +22,4 @@ try:
 
 except ValueError:
     # This catches the error if the user types words instead of numbers
-    print("Error: Please enter a valid whole number.")# Determine student grades based on their test marks
-
-try:
-    marks = int(input("What marks did you get? "))
-    
-    if marks < 0 or marks > 100:
-        print("Please enter a valid mark between 0 and 100.")
-    else:
-        if marks >= 80:
-            grade, message = "A", "You have passed the test"
-        elif marks >= 60:
-            grade, message = "B", "You can do better"
-        elif marks >= 40:
-            grade, message = "C", "You need to improve"
-        else:
-            grade, message = "D", "You have failed"
-
-        print(f"{message}, you got {grade}.")
-
-except ValueError:
-    print("Invalid input! Please enter a whole number.")
+    print("Error: Please enter a valid whole number.")
